b4b6dbeb767ad6aa73a064a3a1157dca79da99f6
shilpa-61 <shilpa.dataexpert@gmail.com>
2026-03-11 23:22:24 +0530

    Rename class from helloWords to hellowords

--- hellowords.java
+++ hellowords.java
@@ -1,4 +1,4 @@
-public class helloWords {
+public class hellowords {
     public static void main(String[] args) {
         System.out.println("Hello Word");
         System.out.println("shilpa devops engineer in azentio company");
